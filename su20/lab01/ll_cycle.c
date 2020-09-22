ccc948944b0e875555d6de00a94a250bc13024ed
xuzheng465 <xuzheng465@gmail.com>
2020-09-23 04:19:33 +1000

    finish cs61c lab1

--- su20/lab01/ll_cycle.c
+++ su20/lab01/ll_cycle.c
@@ -1,7 +1,19 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
-int ll_has_cycle(node *head) {
+int ll_has_cycle(node *head)
+{
     /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+    if (head == NULL)
+        return 0;
+    while (hare->next->next != NULL)
+    {
+        if (hare == tortoise)
+            return 1;
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+    }
     return 0;
 }
